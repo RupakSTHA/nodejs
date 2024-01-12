43dd0191c0b5f89b068c4d185dd2a581d251335a
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2024-01-12 12:17:21 +0545

    Create notes.js

---
+++ javascript-needed/exports/notes.js
@@ -0,0 +1,13 @@
+const getNotes = function(){
+  return 'Your Notes...';
+}
+
+const printNotes = function(){
+  return 'Printing Notes';
+}
+
+//multiple exports from object
+module.exports = {
+  getNotes,
+  printNotes
+}
