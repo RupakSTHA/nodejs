f839aa402d7b53ea71669eff3e5c37f1aa5b03e0
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2024-01-12 11:47:40 +0545

    Update basic.js

--- core-modules/files/basic.js
+++ core-modules/files/basic.js
@@ -1,1 +1,9 @@
+/******
+Command: node basic.js
+****/
 
+//importing file system core modules
+const fs = require('fs');
+
+//notes.txt file created with text
+fs.writeFileSync('notes.txt', 'My first file created');
