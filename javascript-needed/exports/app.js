8293911df037c31de00fd33654c8dd51644dae8d
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2024-01-12 12:21:00 +0545

    Update app.js

--- javascript-needed/exports/app.js
+++ javascript-needed/exports/app.js
@@ -1,10 +1,15 @@
 //importing notes.js by require. get a function in variable. get multiple export way
-const getNotes = require('./notes.js')
+//without destructuring
+const notes = require('./notes.js')
+const msg = notes.getNotes();
+
+//with destructuring
+const {getNotes, printNotes} = require('./notes.js');
+const msg = getNotes();
+//importing notes.js by require. get a function in variable. get multiple export way
 
 //get single export way.
 const name = require('./name.js')
 
-
-const msg = getNotes();
 console.log(msg)
 console.log(name)
