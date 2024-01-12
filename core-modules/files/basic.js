e4fb45cea4f925119e888fd676aac133bb8e04bc
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2024-01-12 11:51:54 +0545

    Update basic.js

--- core-modules/files/basic.js
+++ core-modules/files/basic.js
@@ -5,5 +5,9 @@ Command: node basic.js
 //importing file system core modules
 const fs = require('fs');
 
-//notes.txt file created with text
+//notes.txt file created with text.
+//writeFile will overwrite when executed over previous content.
 fs.writeFileSync('notes.txt', 'My first file created');
+
+//content will be appended in new line in notes.txt
+fs.appendFileSync('notes.txt', '\n My Second line Appended');
